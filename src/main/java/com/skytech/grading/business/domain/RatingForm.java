db4fd04552a2fd04e63c89e356c610b634bdbb71
Y1714202495 <1714202495@qq.com>
2019-06-14 11:43:48 +0800

    1

---
+++ src/main/java/com/skytech/grading/business/domain/RatingForm.java
@@ -0,0 +1,49 @@
+package com.skytech.grading.business.domain;
+
+public class RatingForm {
+    private String ratingFormId;
+    private String level; //等级
+    private String sort; //类别
+    private String condition; //条件
+    private String score; //分数
+
+    public String getRatingFormId() {
+        return ratingFormId;
+    }
+
+    public void setRatingFormId(String ratingFormId) {
+        this.ratingFormId = ratingFormId;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+}
